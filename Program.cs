70e93b9e569601f4c63ab4da3b1194ee4716bc98
Hilbnwal <103248931+Hilbnwal@users.noreply.github.com>
2022-09-07 19:59:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TaskGenerator taskGenerate = new();
+			Task<string>[] tasks = new Task<string>[10];
+
+			for (int i = 0; i < 10; i++)
+			{
+				tasks[i] = taskGenerate.create($"Task - {i + 1}");
+			}
+			Parallel.ForEach(tasks, task =>
+			{
+				task.Start();
+			}
+			);
+
+			Console.WriteLine("\nИтог: " + tasks[Task.WaitAny(tasks)].Result);
+		}
+	}
+	class TaskGenerator
+	{
+		private readonly Random random = new Random();
+		private const int minDelay = 6, maxDelay = 222;
+
+
+		public Task<string> create(string tag)
+		{
+			var delay = random.Next(minDelay, maxDelay);
+			return getTag(tag, delay);
+		}
+
+		private Task<string> getTag(string tag, int delay)
+		{
+			Console.WriteLine($"---> {tag} has delay: {delay}");
+			return new Task<string>(() =>
+			{
+				Thread.Sleep(delay);
+				Console.WriteLine($"---> {tag} executed delay: {delay}");
+				return tag;
+			}
+			);
+
+
+		}
+	}
+}
